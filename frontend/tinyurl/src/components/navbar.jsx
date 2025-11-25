e8e7455375a3ea29471b187baac4ccdce04d84a2
SriramKapaganti <koushiksriram3967@gmail.com>
2025-11-25 21:24:23 +0530

    frontend url fix

--- frontend/tinyurl/src/components/navbar.jsx
+++ frontend/tinyurl/src/components/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 
   const handleLogout = async () => {
     try {
-      await fetch("http://localhost:3000/api/auth/logout", {
+      await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/auth/logout`, {
         method: "GET",
         credentials: "include",
       });
